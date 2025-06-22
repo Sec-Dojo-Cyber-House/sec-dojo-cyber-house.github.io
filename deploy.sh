4fd41f8ec1d3cf18924deef8689a5dedce477b07
Karina Gante <karina.g@aluno.ifsp.edu.br>
2025-06-22 04:13:59 -0300

    Update Blog Auto Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
-WORKTREE_DIR="public"
+#!/bin/bash
+set -e
 
+WORKTREE_DIR="public"
 cd "$WORKTREE_DIR" || exit 1
 
-if git diff --quiet && git diff --cached --quiet; then
+if [ -z "$(git status --porcelain)" ]; then
   echo "Nenhuma mudança detectada. Nenhum commit/push necessário."
+  cd -
   exit 0
 fi
 
 BUILD_NUM=$(git rev-list --count HEAD)
 
-git add .
+git add -A 
 git commit -m "blog build #$((BUILD_NUM + 1))"
 
 git push origin gh-pages
