fb6be1b0b878f6a3333424661f5760f2f398cf78
Karina Gante <karina.g@aluno.ifsp.edu.br>
2025-07-04 20:30:16 -0300

    Update deploy for usual error

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,10 @@ BUILD_NUM=$(git rev-list --count HEAD)
 git add -A 
 git commit -m "blog build #$((BUILD_NUM + 1))"
 
-git push origin gh-pages
+echo "Tentando realizar push para gh-pages..."
+if ! git push origin gh-pages; then
+  echo "Push falhou. Forçando push com --force..."
+  git push origin gh-pages --force
+fi
 
 cd -
